cd1fbf9f419396b05b46ea56baee5c8046bd0dee
unknown <noha.hussan@gmail.com>
2019-02-21 08:36:22 -0500

    submit Lab24

---
+++ ItemAdmin/Controllers/itemsController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ItemAdmin.Models;
+
+namespace ItemAdmin.Controllers
+{
+    public class itemsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: items
+        public ActionResult Index()
+        {
+            return View(db.items.ToList());
+        }
+
+        // GET: items/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: items/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Description,Quantity,Price")] item item)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.items.Add(item);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+            }
+             catch(Exception )
+            {
+                db.items.Remove(item);
+                db.SaveChanges();
+                ViewBag.Error = "ERROR!! this item is already exist in the menu";
+                return View();
+
+            }
+               
+               
+            
+            
+
+
+            return View(item);
+        }
+
+        // GET: items/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: items/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Name,Description,Quantity,Price")] item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: items/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            item item = db.items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            item item = db.items.Find(id);
+            db.items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
